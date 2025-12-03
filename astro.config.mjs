0676f766c3db9dbcf4ab9c0d875a3af102debc00
Aayan Mateen <justaayan@gmail.com>
2025-12-04 01:21:00 +0530

    chore: fix astro adapters

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
+import vercel from '@astrojs/vercel/serverless';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
+const adapter = process.env.VERCEL ? vercel() : netlify();
+
 export default defineConfig({
+  adapter,
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
